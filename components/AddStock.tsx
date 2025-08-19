8131d77bed121c562bd05e55c3028d303386d769
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-19 19:01:55 +0200

    Update AddStock.tsx

--- components/AddStock.tsx
+++ components/AddStock.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Plus, Package, AlertCircle } from 'lucide-react'
 import { inventoryService, expensesService } from '../lib/supabase'
 
