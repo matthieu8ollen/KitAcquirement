171d11e017e47b234712d7b48a37c9352f572b43
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-19 17:18:39 +0200

    Create add-stock.tsx

---
+++ pages/add-stock.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import AddStock from '../components/AddStock'
+
+const AddStockPage: React.FC = () => {
+  return (
+    <Layout>
+      <AddStock />
+    </Layout>
+  )
+}
+
+export default AddStockPage
