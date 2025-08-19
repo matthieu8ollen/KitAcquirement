3b56a9f414973f3e08c7e8a7a4d31a9f2abb1280
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-19 17:17:34 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Dashboard from '../components/Dashboard'
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  )
+}
+
+export default HomePage
