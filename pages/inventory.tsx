28791f3f2f7f6099999dd0264d0644ce96b5a702
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-19 17:18:07 +0200

    Create inventory.tsx

---
+++ pages/inventory.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import InventoryGrid from '../components/InventoryGrid'
+
+const InventoryPage: React.FC = () => {
+  return (
+    <Layout>
+      <InventoryGrid />
+    </Layout>
+  )
+}
+
+export default InventoryPage
